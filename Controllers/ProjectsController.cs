72dceeaecefea54ee789de3c3db0b039e7a4537d
BEAR80416@ICLOUD.COM <BEAR80416@ICLOUD.COM>
2020-10-15 01:59:12 +0800

    up

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myspace
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         public IActionResult Apple()
+        {
+            return View();
+        }
+         public IActionResult Calculator()
+        {
+            return View();
+        }
+         public IActionResult Calendar()
+        {
+            return View();
+        }
+        public IActionResult GuessTheNumber()
+        {
+            return View();
+        }
+        public IActionResult Pizza()
+        {
+            return View();
+        }
+          public IActionResult Puzzle()
+        {
+            return View();
+        }
+         public IActionResult Statistics()
+        {
+            return View();
+        }
+    }
+}
--- wwwroot/js/Home/Layout.js
+++ wwwroot/js/Home/Layout.js
@@ -3,7 +3,7 @@ var c = 1;
 setInterval("changeImg()", "5000");
 
 function changeImg() {
-    selfie.innerHTML = `<img src="https://raw.githubusercontent.com/bear80416/HenryWuCompany/master/HenryWuCompany/Assets/Img/Index/${c}.jpg" alt="">`;
+    selfie.innerHTML = `<img src="../img/index/${c}.jpg" alt="">`;
     c = c + 1;
     if (c == 3) {
         c = c - 2;
